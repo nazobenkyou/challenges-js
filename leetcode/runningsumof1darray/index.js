e910f88a4f832d2b9ae536e9bef40260e2f5d181
Victor Martin Alonso <victor.martin@orangex.es>
2020-09-03 21:30:07 +0200

    chore: running sum 1d array

---
+++ leetcode/runningsumof1darray/index.js
@@ -0,0 +1,47 @@
+/*
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+
+Example 3:
+
+Input: nums = [3,1,2,10,1]
+Output: [3,4,6,16,17]
+
+
+
+Constraints:
+
+    1 <= nums.length <= 1000
+    -10^6 <= nums[i] <= 10^6
+
+ */
+
+const runningSum = (nums) => {
+    let z = 0
+    return nums.map(e => z += e)
+}
+
+const main = () => {
+    console.log(runningSum([1, 2, 3, 4]))
+    console.log(runningSum([1,1,1,1,1]))
+    console.log(runningSum([3,1,2,10,1]))
+    console.log(runningSum([3]))
+    console.log(runningSum([0]))
+    console.log(runningSum([1]))
+    console.log(runningSum([-1, -2, 3, 6]))
+}
+
+main()
